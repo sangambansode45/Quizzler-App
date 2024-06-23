d8a0e281e0726cbe90399087bffee4bd395a168b
sangambansode45 <bansodesangam23@gmail.com>
2024-06-23 20:12:12 +0530

    Add files via upload

---
+++ Quizzler app/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Apple on 18/06/24.
+
+
+import Foundation
+
+
+struct Question {
+    let text : String
+    let answer : String
+    
+    init(q:String,a:String) {
+        text = q
+        answer = a
+    }
+    
+}
+
